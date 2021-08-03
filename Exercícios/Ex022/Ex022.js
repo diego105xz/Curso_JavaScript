90da92741ac329013ef737f65fcadf65af2506c1
Diego Melo <diego-melo@uni9.edu.br>
2021-08-03 13:34:59 -0300

    Últimos Exercícios

---
+++ Exercícios/Ex022/Ex022.js
@@ -0,0 +1,19 @@
+/*
+function parimpar(n){
+    if(n%2 == 0){
+        return 'Par!'
+    } else{
+        return 'Ímpar'
+    }
+}
+
+let res = parimpar (4)
+console.log(res)
+
+*/
+
+function soma(n1, n2){ //podemos passar parametros n1 e n2 valor 0, assim no console caso só inserirmo um valor, não dará erro.
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
